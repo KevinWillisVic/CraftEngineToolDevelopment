f01f1c7054306dab7a6048140da5383997800551
LILKEVS\kevin <kevin.willis.andrew@gmail.com>
2026-02-06 09:05:29 -0800

    Update installer script.

--- Assets/FishAndChips/Code/Crafting/Loading/CraftingSystemGameInstaller.cs
+++ Assets/FishAndChips/Code/Crafting/Loading/CraftingSystemGameInstaller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b178182f1ae59059c4d6e7a188363fa1b76449cbb53fe41d7b4769158d2a5b4
-size 6711
+oid sha256:a1791739c5efc99e81af5930978cb6e69a0afc7dcbcb5179d91892f4cbd17a31
+size 6725
