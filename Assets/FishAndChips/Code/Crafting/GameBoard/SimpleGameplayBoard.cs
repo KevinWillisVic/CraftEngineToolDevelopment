ecefea2d425ba6dac14467f532de1352d6ca9585
LILKEVS\kevin <kevin.willis.andrew@gmail.com>
2026-02-27 13:39:48 -0800

    fix clean up button

--- Assets/FishAndChips/Code/Crafting/GameBoard/SimpleGameplayBoard.cs
+++ Assets/FishAndChips/Code/Crafting/GameBoard/SimpleGameplayBoard.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd8e21e3a63b918d76bdb20fe0ab9c80ee4fba29ba81233fa196c3d0d7348102
-size 5469
+oid sha256:2fdc11efc4e560ec6be509238cad3be5e03b317a052b4b6d9a56c56ae60ca301
+size 5371
--- Assets/FishAndChips/Code/Crafting/UI/Overlays/OverlaySettings.cs
+++ Assets/FishAndChips/Code/Crafting/UI/Overlays/OverlaySettings.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05c7b5038a42a0e6cea610621f55ad3ff3e1fa20f47f50005ddf43a12743489a
-size 3544
+oid sha256:869045aa4c4ac8f76adeb1676a752bddd2c71355251b15a13198c5284e55b33f
+size 3555
