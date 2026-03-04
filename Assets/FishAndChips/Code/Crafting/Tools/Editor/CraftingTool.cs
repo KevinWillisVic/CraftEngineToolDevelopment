8d8f8dd066f435ce39bebca64f5cc46936584162
LILKEVS\kevin <kevin.willis.andrew@gmail.com>
2026-03-04 09:44:55 -0800

    firebase package

--- Assets/FishAndChips/Code/Crafting/Tools/Editor/CraftingTool.cs
+++ Assets/FishAndChips/Code/Crafting/Tools/Editor/CraftingTool.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efe722bc4549701b8e13131ec90ad16bb88e5196ef08fe101b297e02109355ea
-size 894
+oid sha256:ca4342643ea3757e4c33a7e497a933714fb87df3538550098fe5f981edb355a4
+size 1238
