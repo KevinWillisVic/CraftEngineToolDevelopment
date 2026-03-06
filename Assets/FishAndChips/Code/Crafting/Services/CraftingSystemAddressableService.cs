09db4b3a4fd7a55d65d10cb128208c0f26e812a3
LILKEVS\kevin <kevin.willis.andrew@gmail.com>
2026-03-05 20:55:10 -0800

    Test commit

--- Assets/FishAndChips/Code/Crafting/Services/CraftingSystemAddressableService.cs
+++ Assets/FishAndChips/Code/Crafting/Services/CraftingSystemAddressableService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a0ba014c712c814fe8afe4b22f80d1b2be5762bad586397951277b70726040c
-size 7570
+oid sha256:1f20a0e30ff8fe837a762aabf0bc3d582c042875e9e42bc3e726ae280a652d39
+size 7571
